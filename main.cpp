7646038c27d7ea284a94c79b538ff1cf9f81fa78
davidbrill <131848212+davidbrill@users.noreply.github.com>
2023-04-26 13:29:31 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout<<"Hallo, ini program pertamaku";
+	cout<<endl;
+	cout<<"Saya Belajar Bahasa C++";
+	return 0;
+}
